ef8f1c38996b69a4dfe5372415d6d59a70f8c1a0
Daiyu Hatakeyama <dahatake@hotmail.com>
2020-04-15 18:08:12 +0900

    Initial Update
    Console base StreamingEndpoint Control code by C#

---
+++ StreamingEndpointAutoscaleSample/AzureMediaServices-StreamingEndpoint-AutoScale/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.Azure.Management.Media;
+using Microsoft.Azure.Management.Media.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Rest;
+using Microsoft.Rest.Azure.Authentication;
+
+namespace AzureMediaServices_StreamingEndpoint_AutoScale
+{
+    class Program
+    {
+
+        static string _defaultStreamingEndpointName = "default";
+        static int _defaultChangeValue = 1;
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Start Job");
+
+            AzureMediaServicesConfig config = new AzureMediaServicesConfig(
+                new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build());
+
+            try
+            {
+                Console.WriteLine("1. Login Azure Media Services");
+                IAzureMediaServicesClient client = await CreateMediaServicesClientAsync(config);
+                Console.WriteLine("connected");
+
+                Console.WriteLine("2. Change StreamingEndpoint ScaleUnit");
+                var streamingEndpoint = client.StreamingEndpoints.Get(config.ResourceGroup,
+                    config.AccountName,
+                    _defaultStreamingEndpointName);
+
+                Console.WriteLine($"  StreamingEndpoint-Before #{streamingEndpoint.ScaleUnits}");
+                var NewScaleUnitNumber = streamingEndpoint.ScaleUnits + _defaultChangeValue;
+                await client.StreamingEndpoints.ScaleAsync(
+                    config.ResourceGroup, 
+                    config.AccountName, 
+                    _defaultStreamingEndpointName,
+                    NewScaleUnitNumber);
+                Console.WriteLine($"  StreamingEndpoint-After  #{NewScaleUnitNumber}");
+
+                client.Dispose();
+                Console.WriteLine("Completed!");
+
+            }
+            catch (Exception exception)
+            {
+                if (exception.Source.Contains("ActiveDirectory"))
+                {
+                    Console.Error.WriteLine("Tip: Make sure that you have filled out the appsettings.json file before running this sample.");
+                }
+
+                Console.Error.WriteLine($"{exception.Message}");
+
+                ApiErrorException apiException = exception.GetBaseException() as ApiErrorException;
+                if (apiException != null)
+                {
+                    Console.Error.WriteLine(
+                        $"ERROR: API call failed with error code '{apiException.Body.Error.Code}' and message '{apiException.Body.Error.Message}'.");
+                }
+            }
+        }
+
+        private static async Task<ServiceClientCredentials> GetCredentialsAsync(AzureMediaServicesConfig config)
+        {
+            // Use ApplicationTokenProvider.LoginSilentWithCertificateAsync or UserTokenProvider.LoginSilentAsync to get a token using service principal with certificate
+            //// ClientAssertionCertificate
+            //// ApplicationTokenProvider.LoginSilentWithCertificateAsync
+
+            // Use ApplicationTokenProvider.LoginSilentAsync to get a token using a service principal with symmetric key
+            ClientCredential clientCredential = new ClientCredential(config.AadClientId, config.AadSecret);
+            return await ApplicationTokenProvider.LoginSilentAsync(config.AadTenantId, clientCredential, ActiveDirectoryServiceSettings.Azure);
+        }
+
+        private static async Task<IAzureMediaServicesClient> CreateMediaServicesClientAsync(AzureMediaServicesConfig config)
+        {
+            var credentials = await GetCredentialsAsync(config);
+
+            return new AzureMediaServicesClient(config.ArmEndpoint, credentials)
+            {
+                SubscriptionId = config.SubscriptionId,
+            };
+        }
+    }
+}
---
+++ StreamingEndpointAutoscaleSample/AzureMediaServices-StreamingEndpoint-AutoScale/README.md
@@ -0,0 +1,12 @@
+# 必要なもの
+- Visual Studio Code
+- .NET Core
+
+# 実行手順
+
+## 1. Azure Media Services のログイン情報 json ファイル作成
+
+`appsettings.json` ファイルを生成します。Azure Portal から作成できます。
+
+- To Be Updated
+
